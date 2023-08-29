823a6037a07f97544aaf5b26b8247d1659cb1ae6
heclair <heclair@hotmail.com>
2023-08-28 22:10:55 -0300

    aula 28/08

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express from "express";
+import{sum,dif} from "./operacao"
+import dotenv from "dotenv";
+import { dayOfMonth, month, year } from "./date";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.listen(3001);
+app.listen(PORT, function(){console.log(`Rodando ${PORT}`)});
 
-app.get("/somar/:w/:z", function(req,res){
-    const{w,z} = req.params;
-    let r = parseFloat(w) + parseFloat (z);
-    res.send({r});
-});
+app.get("/somar/:x/:y", sum);
 
-app.post("/subtrair", function(req,res){
-    const{x,y} = req.body;
-    let r = parseFloat(x) - parseFloat (y);
-    res.send({r});
-});
+app.post("/subtrair", dif);
+app.get("/dia", dayOfMonth);
+app.get("/mes", month);
+app.get("/ano", year);
